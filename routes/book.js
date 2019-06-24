5931b2487aed67525dcbf333fe04d8a45007c856
Caterina Clayton <caterina.clayton@gmail.com>
2019-06-23 22:37:07 -0500

    booking added to mongoose

--- routes/book.js
+++ routes/book.js
@@ -3,30 +3,30 @@ const router = express.Router();
 const bodyParser = require('body-parser');
 // Booking model
 const Booking = require('../models/Booking');
+
 router.get("/",function(req,res){
-  res.render("book.pug");
+    res.render("book.pug");
 });
 
-////
-// router.use(bodyParser());
- // as only one page can use res.sendfile to render the page which will 
- // contain the dropdowns ...
-// router.get('/', function(req, res){
-//   res.sendfile('./views/book.pug');
-// });
+router.post('/getJson', async function (req, res) {
+   //console.log(req.body.genre);
+   //res.send('yo');
+   const genre = req.body.genre;
+   const payment = req.body.payment;
+   const location= req.body.location;
 
-router.post('/getJson', function (req, res) {
-   console.log(req.body.genre);
-   res.send('yo');
+   let booking = new Booking({
+    genre:genre,
+    payment:payment, 
+    location:location
+    
+  });
+  
+  await booking.save();
+    res.send('Your appointment has been requested.');
+    
 });
  
-module.exports = router;
-// let booking = new Booking({
-  
-//   genre: genre,
-//   payment: payment, 
-//   location: location,
-  
-// });
 
-// booking.save()
+
+module.exports = router;
